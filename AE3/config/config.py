72cb5d2cdba2922b90c7000d325a6f73ae589304
Akshay Mehta <akshay@otainfo.com>
2025-08-18 17:53:09 -0700

    Added config file to persist credentials

---
+++ AE3/config/config.py
@@ -0,0 +1,83 @@
+# OTAinfo, Inc - By: Akshay - Mon Aug 18 2025
+import json
+import os
+
+class config:
+
+    def __init__(self, passcode):
+        self.config = {}
+        self.passcode = passcode
+
+
+    def loadconfig(self):
+
+        filefound = False
+        try:
+            _temp = open('config.json', 'r')
+            filefound = True
+        except:
+            pass
+        if filefound:
+            config_file = open('./config/config.json', 'r')
+            secured_config_file_contents = config_file.read()
+            config_file_contents = self.decrypt_contents(secured_config_file_contents)
+            config_file.close()
+            if(config_file_contents):
+                self.config = eval(config_file_contents)
+                if len(self.config) > 0:
+                    print("Config loaded")
+
+    def decrypt_contents(self, contents):
+        return contents
+
+    def getconfig(self):
+        return self.config
+
+    def saveconfig(self, contents):
+        print('Saving Config ... ')
+        print(self.config)
+        print(contents)
+        if (self.config != contents):
+            print('Contents Updated.. ')
+            keys_added = set(contents.keys()) - set(self.config.keys())
+            keys_removed = set(self.config.keys()) - set(contents.keys())
+            if keys_removed:
+                print(keys_removed)
+                for key in keys_removed:
+                    newkey = key + 'old'
+                    self.config[newkey] = self.config[key]
+            if keys_added:
+                print(keys_added)
+                for key in keys_added:
+                    self.config[key] = contents[key]
+
+            for key in contents.keys():
+                if self.config[key] != contents[key]:
+                   print('Contents changed for key: ' + key + ' old: ' + self.config[key] + '  new: ' + contents[key])
+                   newkey = 'ov' + key
+                   self.config[newkey] = self.config[key]
+                   self.config[key] = contents[key]
+
+       #Type Error: Object with buffer protocol required
+       #config_file = open('./config/config.json', 'w')
+       # config_file.write(self.config)
+       # config_file.close()
+
+class st:
+
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            # If no instance exists, create a new one using the parent's __new__
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self, config):
+       if not hasattr(self, '_initialized'):  # Prevent re-initialization on subsequent calls
+           print("st initialized!")
+           self._initialized = True
+           self.config = config
+
+    def getcacheconfig(self):
+        return self.config
--- AE3/main.py
+++ AE3/main.py
@@ -4,12 +4,27 @@ from wifi.wifi_connection import wifi_connect
 from otainfo_apis import apicall
 import asyncio
 import time
+import omv
+import machine
+import json
+from config import config
 
-
-con = wifi_connect('', '')
-con.connect()
-while not con.wlan.isconnected():
-    time.sleep(1)
+cfg = config.config('')
+cfg.loadconfig()
+cfgdata = cfg.getconfig()
+print(omv.board_id());
+print(omv.arch())
+print(omv.version_string())
+print(machine.unique_id())
+byte_string = machine.unique_id()
+decimal_value = int.from_bytes(byte_string)
+try:
+    con = wifi_connect(cfgdata['wifi_username'], cfgdata['wifi_password'])
+    con.connect()
+    while not con.wlan.isconnected():
+        time.sleep(1)
+except:
+    print('wifi credentials absent')
 
 
 nobj =  {  'url' : 'https://otainfo.us:9001/',
@@ -28,5 +43,3 @@ nobj =  {  'url' : 'https://otainfo.us:9001/',
 api_req = apicall.api_request(nobj)
 #api_req.make_post_call()
 asyncio.run(api_req.make_post_call())
-
-
